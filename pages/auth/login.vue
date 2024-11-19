0070f1dc4a691aa19dc49c69fd37e5a85174772d
rvyhvn <rvyhvn@tuta.io>
2024-11-19 11:48:21 +0800

    add register form

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,13 +2,11 @@
 const supabase = useSupabaseClient();
 const email = ref("");
 const password = ref("");
-const phone = ref("");
 
 const signInWithPassword = async () => {
 	const { error } = await supabase.auth.signInWithPassword({
 		email: email.value,
 		password: password.value,
-		phone: phone.value,
 	});
 	if (error) console.log(error);
 };
@@ -19,6 +17,5 @@ const signInWithPassword = async () => {
 		<button @click="signInWithPassword">Sign In with E-Mail</button>
 		<input v-model="email" type="email" />
 		<input v-model="password" type="text" />
-		<input v-model="phone" type="number" />
 	</div>
 </template>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
-const hi = "Hi!";
+const supabase = useSupabaseClient();
+const email = ref("");
+const password = ref("");
+const phone = ref("");
+
+const signUpWithPassword = async () => {
+	const { error } = await supabase.auth.signUp({
+		email: email.value,
+		password: password.value,
+		phone: phone.value,
+	});
+	if (error) console.log(error);
+};
 </script>
 
 <template>
-	<h1>{{ hi }}</h1>
+	<div>
+		<button @click="signUpWithPassword">Sign Up</button>
+		<input v-model="email" type="email" />
+		<input v-model="password" type="text" />
+		<input v-model="phone" type="number" />
+	</div>
 </template>
