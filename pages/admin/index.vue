9b94bda6082416496dd3e3d26201b76870a16246
rvyhvn <rvyhvn@tuta.io>
2024-11-19 11:31:20 +0800

    add admin, login and register page

---
+++ pages/admin/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const helloWorld = "Hello, world";
+</script>
+
+<template>
+	<h1>{{ helloWorld }}</h1>
+</template>
---
+++ pages/auth/login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const hi = "Hi!";
+</script>
+
+<template>
+	<h1>{{ hi }}</h1>
+</template>
